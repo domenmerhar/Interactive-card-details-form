2e0d641570643a70aeb75e6e6f66d298dd3feeef
domenmerhar <merhardomen@gmail.com>
2023-11-11 08:35:47 +0100

    Revert "Added error html and css"
    This reverts commit f2fff87e818d93f5e60cdf1162467756ccf1170d.

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const trackedFields = [
     document.querySelector('[data-input="exp-date-year"]'),
   ],
   [
-    document.querySelector('[data-text="cvc"]'),
-    document.querySelector('[data-input="cvc"]'),
+    document.querySelector('[data-text="CVC"]'),
+    document.querySelector('[data-input="CVC"]'),
   ],
 ]
 
